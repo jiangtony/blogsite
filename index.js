d3cae14a48bf30f04aceffd0a7961808f1a7d24e
jiangtony <tony.jiang34@gmail.com>
2018-01-19 23:13:27 -0500

    Finished adding routes
    Added edit, update, and destroy routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express"),
 		app = express(),
 		bodyParser = require("body-parser"),
-		mongoose = require("mongoose");
+		mongoose = require("mongoose"),
+		methodOverride = require("method-override");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost/blogsite");
 
@@ -21,9 +23,9 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-///////////
+///////////////
 // ROUTES
-///////////
+///////////////
 
 app.get("/", function(req, res) {
 	res.redirect("/blogs");
@@ -69,10 +71,38 @@ app.get("/blogs/:id", function(req, res) {
 });
 
 
+// EDIT route
+app.get("/blogs/:id/edit", function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	})
+});
 
+// UPDATE route
+app.put("/blogs/:id", function(req, res) {
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
 
-
-
+// DESTROY route
+app.delete("/blogs/:id", function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
 
 app.listen(3000, function() {
 	console.log("working");
